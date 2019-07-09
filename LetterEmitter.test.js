df18d5356efff3f5d7b8d54853fe7dd89edbfdb6
sepuckett86 <sepuckett86@gmail.com>
2019-07-09 15:24:07 -0700

    refactor test

--- LetterEmitter.test.js
+++ LetterEmitter.test.js
@@ -1,12 +1,13 @@
 const LetterEmitter = require('./LetterEmitter');
 
 describe('Letter Emitter', () => {
-  it('emits an event for each letter in a string upon call of read method', () => {
+  it('emits an event for each letter in a string upon call of read method', done => {
     const letterEmitter = new LetterEmitter();
     const str = 'hello there';
     letterEmitter.on('event', letter => {
       expect(str).toContain(letter);
     });
+    letterEmitter.on('end', done);
     letterEmitter.read(str);
   });
 });
