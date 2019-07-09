a603b8bfd5df4ebca530744f9528272dbfdef645
sepuckett86 <sepuckett86@gmail.com>
2019-07-09 16:06:25 -0700

    add jest.fn() test

--- LetterEmitter.js
+++ LetterEmitter.js
@@ -2,7 +2,7 @@ const EventEmitter = require('events');
 
 class LetterEmitter extends EventEmitter {
   read(str) {
-    str.split().forEach((letter, offset) => {
+    str.split('').forEach((letter, offset) => {
       this.emit('item', {
         letter,
         offset
--- LetterEmitter.test.js
+++ LetterEmitter.test.js
@@ -1,8 +1,12 @@
 const LetterEmitter = require('./LetterEmitter');
 
 describe('Letter Emitter', () => {
-  it('emits an event for each letter with data in format { letter: "b", offset: 20 }', done => {
-    const letterEmitter = new LetterEmitter();
+  let letterEmitter;
+  beforeEach(() => {
+    letterEmitter = new LetterEmitter();
+  });
+
+  it('emits data in format { letter: "b", offset: 20 }', done => {
     const str = 'hello there';
     letterEmitter.on('item', data => {
       expect(data).toEqual(expect.objectContaining({
@@ -11,6 +15,19 @@ describe('Letter Emitter', () => {
       }));
     });
     letterEmitter.on('end', done);
+    letterEmitter.read(str);
+  });
+
+  it('emits the correct number of times (for each letter)', done => {
+    const str = 'hello there';
+    const mockCallback = jest.fn();
+    letterEmitter.on('item', () => {
+      mockCallback();
+    });
+    letterEmitter.on('end', () => {
+      expect(mockCallback.mock.calls.length).toBe(str.length);
+      done();
+    });
     letterEmitter.read(str);
   });
 });
