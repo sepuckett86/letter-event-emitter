e421fc796174b945b31663e485a1503d812e23ea
sepuckett86 <sepuckett86@gmail.com>
2019-07-09 15:33:49 -0700

    redo test

--- LetterEmitter.test.js
+++ LetterEmitter.test.js
@@ -1,11 +1,14 @@
 const LetterEmitter = require('./LetterEmitter');
 
 describe('Letter Emitter', () => {
-  it('emits an event for each letter in a string upon call of read method', done => {
+  it('emits an event for each letter with data in format { letter: "b", offset: 20 }', done => {
     const letterEmitter = new LetterEmitter();
     const str = 'hello there';
-    letterEmitter.on('item', letter => {
-      expect(str).toContain(letter);
+    letterEmitter.on('item', data => {
+      expect(data).toEqual(expect.objectContaining({
+        letter: expect.any(String),
+        offset: expect.any(Number),
+      }));
     });
     letterEmitter.on('end', done);
     letterEmitter.read(str);
