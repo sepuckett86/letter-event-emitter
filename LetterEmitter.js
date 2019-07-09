9b6d20448d2ad224bdf51e1739d63b92c75a0281
sepuckett86 <sepuckett86@gmail.com>
2019-07-09 15:35:14 -0700

    pass test

--- LetterEmitter.js
+++ LetterEmitter.js
@@ -2,8 +2,11 @@ const EventEmitter = require('events');
 
 class LetterEmitter extends EventEmitter {
   read(str) {
-    str.split().forEach(letter => {
-      this.emit('item', letter);
+    str.split().forEach((letter, offset) => {
+      this.emit('item', {
+        letter,
+        offset
+      });
     });
     this.emit('end');
   }
